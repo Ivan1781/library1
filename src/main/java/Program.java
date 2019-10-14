dc0805598cad9d9105868005701b282966a57536
sobolev.ivan901 <sobolevi1781@gmail.com>
2019-10-15 00:56:11 +0300

    add spring boot, Controller class ...

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,60 +1,20 @@
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.query.Query;
-import org.hibernate.query.sqm.tree.predicate.LikeSqmPredicate;
-import org.hibernate.service.Service;
-import org.hibernate.service.ServiceRegistry;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import javafx.print.PageOrientation;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@SpringBootApplication
 public class Program {
+
     public static void main(String[] args) throws Exception {
-
-          AutDao autDao = new AutDao();
-          autDao.remove(1);
-
-//          Author a = new Author(1, "Descartes");
-//          autDao.add(a);
-
-
-
-//
-//        autDao.remove(26);
-//
-//        List<Author> authorw = autDao.getAll();
-//        for(Author a: authorw){
-//            System.out.println(a);
-//        }
+        SpringApplication.run();
 
 
 
 
-//        try {
-//            SessionFactory a = HibernateUtil.buildSessionFactory();
-//            Session session = a.openSession();
-//            Transaction transaction = session.beginTransaction();
-//
-//            Query query = session.createQuery("FROM Title");
-//
-//
-//            List ttt = query.list();
-//            System.out.println(ttt);
-//
-//
-//            session.getTransaction().commit();
-//            session.close();
-//
-//        }
-//        catch (Exception e) {
-//            System.out.println("AAAAAAAAA");
-//
-//
-//        }
+
+
+
     }
 }
