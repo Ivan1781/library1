b78c1cb50beb35ed50c64253ab720ef385f4bd14
IvanS <sobolev-ivan901@yandex.ru>
2019-10-21 01:42:25 +0300

    new pom, controller and view

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,10 +2,10 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-@ComponentScan
+
 public class Program {
 
     public static void main(String[] args) throws Exception {
--- src/main/java/application/control/Controller.java
+++ src/main/java/application/control/Controller.java
@@ -1,14 +1,20 @@
 package application.control;
 
+import application.Entities.Author;
+import application.dao.AutDao;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
+
 public class Controller {
 
     @GetMapping("/index")
-    public String homepage(){
+    public String homepage(Model model) throws Exception {
+        AutDao autDao = new AutDao();
+
+        Author author = autDao.getById(9);
+        model.addAttribute("firstAuthor", author);
         return "index";
-
     }
-
 }
